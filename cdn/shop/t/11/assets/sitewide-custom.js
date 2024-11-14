eb51754e40e464862b4541eab6cd1cb0859e11df
blackhood <54266042+Bl4ckh00d@users.noreply.github.com>
2024-11-14 15:52:31 +0530

    Add files via upload

---
+++ cdn/shop/t/11/assets/sitewide-custom.js
@@ -0,0 +1,2 @@
+$(document).ready(function(){setActiveNavMenuItem(),$("#menu-open").on("click",function(){$("#menu-pane").toggleClass("hidden"),$("html").addClass("overflow-hidden")}),$("#menu-close").on("click",function(){$("#menu-pane").toggleClass("hidden"),$("html").removeClass("overflow-hidden")})});function setActiveNavMenuItem(){var current=location.pathname;$(".nav li a").each(function(){var $this=$(this);$this.attr("href")===current&&$this.parent().addClass("active")})}
+//# sourceMappingURL=/cdn/shop/t/11/assets/sitewide-custom.js.map?v=90987848902489245371723667681
